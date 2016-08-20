3501220267ba2c79595630be5c56688c2d1c6fe2
winstonhoseadriggins <wdriggins@gmail.com>
2016-08-20 08:55:05 -0400

    added node_modules to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,43 @@
 var gulp = require('gulp');
- uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var replace = require('gulp-html-replace');
+var sourcemap = require('gulp-sourcemaps');
 
- gulp.task('scripts', function() {
-     gulp.src('js/*.js')
-     .pipe(uglify())
-     .pipe(gulp.dest('build/js'));
- });
- gulp.task('default', [ 'scripts']);
- 
+gulp.task('content', function(){
+	gulp.src('./src/index.html')
+		.pipe(gulp.dest('./dist/'))
+		.pipe(reload({stream: true}))
+});
+
+gulp.task('scripts', function(){
+	gulp.src('./src/scripts/*.js')
+		.pipe(sourcemap.init())
+			.pipe(uglify())
+			.pipe(concat('app.js'))
+		.pipe(sourcemap.write())
+		.pipe(gulp.dest('./dist/scripts'))
+		.pipe(reload({stream: true}))
+});
+
+gulp.task('styles', function(){
+	gulp.src('./src/styles/*.css')
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('./dist/styles'))
+		.pipe(reload({stream: true}))
+});
+
+gulp.task('serve', function(){
+	browserSync.init({
+		server: {
+			baseDir: './dist/'
+		}
+	});
+gulp.watch('./src/index.html', ['content']);
+	gulp.watch('./src/scripts/*.js', ['scripts']);
+	gulp.watch('./src/styles/*.css', ['styles']);
+});
+gulp.task('default', ['content', 'scripts', 'styles', 'serve']);
---
+++ views/js/gulpfile.js
@@ -0,0 +1,69 @@
+// Include gulp
+var gulp = require('gulp'); 
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+// Minify CSS
+gulp.task('minify-css', function() {
+  return gulp.src('css/*')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Image Optimization
+//gulp.task('imagess', function () {
+//    return gulp.src('img/*')
+//      .pipe(imagemin({
+//          progressive: true,
+//            svgoPlugins: [{removeViewBox: false}],
+//            use: [imageminPngquant()]
+//        }))
+//        .pipe(gulp.dest('dist/images'));
+//});
+
+//gulp.task('quant', function () {
+//    return gulp.src('img/*.png')
+//        .pipe(imageminPngquant({quality: '65-80', speed: 4})())
+//        .pipe(gulp.dest('build/images'));
+//});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch', 'minify-css',]);
